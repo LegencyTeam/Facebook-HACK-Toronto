abf25309ec3c8689d4e0456a721b5db9383d0dfb
LegencyTeam <95074605+LegencyTeam@users.noreply.github.com>
2021-12-09 11:50:23 +0700

    Update RSS.php

--- RSS.php
+++ RSS.php
@@ -1,19 +1,2 @@
-<?php
-
-include realpath($_SERVER['DOCUMENT_ROOT']).'/db.php';
-
-header('P3P: CP="CAO PSA OUR"');
-//header('Content-type: application/rss+xml');
-
-$uid = !empty($_GET['uid']) ? mysql_real_escape_string($_GET['uid']) : '0';
-
-$row = mysql_fetch_assoc(getRSS($uid));
-echo stripslashes($row['rss']);
-
-function getRSS($uid){
-	$sql = "SELECT `uid`,`rss` FROM `slideshow` WHERE `uid`='$uid'";
-	$result = mysql_query($sql) or die ("Select failed: ".mysql_error());
-	return $result;
-}
-
-?>
+อยากได้ก็เก็บตังอย่าโง่ 
+https://www.discord.io/LygencyHeck
